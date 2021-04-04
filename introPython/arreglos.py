47de89b2922519f6cbea1c6f1e88ddff58f9958d
Yeison Montoya <yeisondiseno@gmail.com>
2021-04-04 14:16:56 -0500

    Y.Montoya: finalización de arreglos o diccionarios

--- introPython/arreglos.py
+++ introPython/arreglos.py
@@ -1,13 +1,31 @@
 
-gatitos ={
-    'Fluffy': {
-        'nombre': 'Fluffy',
-        'edad': 4
-    }, 
-    'Mamba': {
-        'nombre': 'Black Mamba',
-        'edad': 12
-    }
+# gatitos ={
+#     'Fluffy': {
+#         'nombre': 'Fluffy',
+#         'edad': 4
+#     }, 
+#     'Mamba': {
+#         'nombre': 'Black Mamba',
+#         'edad': 12
+#     }
+# }
+
+fluffy = {
+    'nombre': 'Fluffy',
+    'edad': 4
 }
 
+mamba = {
+    'nombre': 'Black Mamba',
+    'edad': 12
+}
+
+gatitos = {
+    'Fluffy': fluffy,
+    'Mamba': mamba
+}
 print(gatitos)
+
+# crear un constructor
+perritos = dict(nombre = 'Chanchito Feliz', edad = 6)
+print(perritos)
