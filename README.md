086a98386088972ba7024fb23ed2ff9c410b4e17
Yeison Montoya <yeisondiseno@gmail.com>
2021-04-04 13:16:31 -0500

    Y.Montoya: tipos de datos y elementos, tuplas y listas

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Curso profesional de Phyton 
 ## _Curso impartido por Nicolas Schurmann_
 
-Este curso te comvierte en un profesional en Python.
+Este curso te convierte en un profesional en Python dandote las bases del Back-End y complementando mi conocimiento de Front-End.
 
-## enseña
+## Enseña
 - HTML
 - CSS
 - Python
@@ -20,7 +20,7 @@ Este curso te comvierte en un profesional en Python.
 - RESTFUL API
 
 ## Añade conocimiento 
-Mi conocimiento base como Front-End en React se vera incrementado al finalizar este curso. 
-
+Mi conocimiento base como Front-End en React se verán incrementados al finalizar este curso. 
+ 
 ### _Link_
 [Descripción](https://www.youtube.com/watch?v=p7FAU0IhrrE)
--- introPython/index.py
+++ introPython/index.py
@@ -1,13 +1,45 @@
+# Esto se un comenaio de python 3
 if 6 > 5:
     print('6 es mayor a 5')
 
 x = 5
 y = 'Hola'
+# otro mernsaje
 
-print(x, y)
+# print(x, y)
 
 email = 'yeisondesing@gmail.com'
 
-print(email)
+# print(email)
 
-# este es un mensaje
+# Manejo de listaas 
+lista = [1, 2, 3, 1, 2, 2]
+lista2 = lista.copy()
+lista.append(4)
+# lista.clear()  Esto limpia la listaa
+# print(lista, lista2.count(2))
+# print(len(lista), len(lista2))
+
+listaWords = ['Hola', 'Mundo', 'Soy un Gordito']
+# print(listaWords[2])
+
+# Eliminar elementos 
+# lista.pop() Solo elimina el último elemento
+#  elimina por elemento 
+# listaWords.remove('Hola')
+# print(listaWords)
+
+
+# lista.reverse() revierte el orden de la listaa 
+lista.sort()
+listaWords.sort()
+listaWords.reverse()
+# print(lista, listaWords)
+
+# crear tuplas
+tupla = ('Hola', 'Mundo', 'Somos', 'Tupla')
+print(tupla.index('Mundo'))
+
+# transformamos tupla a listaa
+listaDeTupla = list(tupla)
+print(listaDeTupla)
